5fbdbec443a4249611fd5b1a195c9b830c93a0ea
Diddens, Christian (UT-TNW) <c.diddens@utwente.nl>
2024-05-21 21:32:46 +0200

    added installation page

--- gen_page.sh
+++ gen_page.sh
@@ -1,4 +1,5 @@
 mkdir -p _generated
 cat header.html index.html footer.html > _generated/index.html || exit 1
 cat header.html about.html footer.html > _generated/about.html || exit 1
+cat header.html installation.html footer.html > _generated/installation.html || exit 1
 
