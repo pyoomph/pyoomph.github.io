736521e8c09cb63442d7030eb80b9eb71d009b7d
c.diddens <c.diddens@utwente.nl>
2025-01-20 09:06:48 +0100

    Updated refs

--- gen_page.sh
+++ gen_page.sh
@@ -1,5 +1,5 @@
 mkdir -p _generated
-cat header.html index.html footer.html > _generated/index.html || exit 1
+cat header.html index1.html <(python gen_example_gallery.py || exit 1) index2.html footer.html > _generated/index.html || exit 1
 cat header.html about.html <(python gen_pubs.py || exit 1) footer.html > _generated/about.html || exit 1
 cat header.html installation.html footer.html > _generated/installation.html || exit 1
 
